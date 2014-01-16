3a8e065fdd17fca97609f071251a853ca6d2d288
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-16 18:53:32 +1000

    Plugin nows gets phrases from config.yml. Does not add them to list yet.

--- src/com/bugs3/zewildguy/chatphrases/ChatPhrases.java
+++ src/com/bugs3/zewildguy/chatphrases/ChatPhrases.java
@@ -1,6 +1,7 @@
 package com.bugs3.zewildguy.chatphrases;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,7 +17,23 @@ public class ChatPhrases extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		
+		this.saveDefaultConfig();
+		
+		List<String> listOfStrings = Arrays.asList("Hello World", "Welcome to Bukkit", "Have a Good Day!");
+		this.getConfig().set("phrases", listOfStrings);
+		
 		ChatPhrase.addPhrase("phrase_id", "phrase_content");
 		ChatPhrase.addPhrase("example_phrase", "&3This is an example phrase!");
+		
+		for(String phrase : listOfStrings) {
+			phrase.split("");
+			
+			String phrase_id = null;
+			String phrase_content = null;
+			
+			ChatPhrase.addGlobalPhrase(phrase_id, phrase_content);
+		}
+		
+		
 	}
 }
